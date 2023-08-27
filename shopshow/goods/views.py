a17d6bbeb335a92120cf00949aa585cd420b2577
xl <1176530132@qq.com>
2023-08-27 17:36:29 +0800

    mobile

--- shopshow/goods/views.py
+++ shopshow/goods/views.py
@@ -8,7 +8,7 @@ from django.shortcuts import HttpResponseRedirect
 from django.contrib import messages
 from django.db.models import Q
 from django.shortcuts import redirect
-import csv
+import csv,re
 import time
 import random
 import smtplib
@@ -16,16 +16,70 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.header import Header
 
+
+def judge_pc_or_mobile(ua):
+    """
+    判断访问来源是pc端还是手机端
+    :param ua: 访问来源头信息中的User-Agent字段内容
+    :return:
+    """
+
+    factor = ua
+    is_mobile = False
+
+    _long_matches = r'googlebot-mobile|android|avantgo|blackberry|blazer|elaine|hiptop|ip(hone|od)|kindle|midp|mmp' \
+                    r'|mobile|o2|opera mini|palm( os)?|pda|plucker|pocket|psp|smartphone|symbian|treo|up\.(browser|link)' \
+                    r'|vodafone|wap|windows ce; (iemobile|ppc)|xiino|maemo|fennec'
+    _long_matches = re.compile(_long_matches, re.IGNORECASE)
+    _short_matches = r'1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)' \
+                     r'|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)' \
+                     r'|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw' \
+                     r'|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8' \
+                     r'|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit' \
+                     r'|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)' \
+                     r'|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji' \
+                     r'|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|e\-|e\/|\-[a-w])|libw|lynx' \
+                     r'|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(di|rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi' \
+                     r'|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)' \
+                     r'|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg' \
+                     r'|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21' \
+                     r'|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-' \
+                     r'|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it' \
+                     r'|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)' \
+                     r'|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)' \
+                     r'|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit' \
+                     r'|wi(g |nc|nw)|wmlb|wonu|x700|xda(\-|2|g)|yas\-|your|zeto|zte\-'
+
+    _short_matches = re.compile(_short_matches, re.IGNORECASE)
+
+    if _long_matches.search(factor) != None:
+        is_mobile = True
+    user_agent = factor[0:4]
+    if _short_matches.search(user_agent) != None:
+        is_mobile = True
+
+    return is_mobile
+
 # Create your views here.
 def goods(request):
+    re_title = request.META.get("HTTP_USER_AGENT")
+    is_mobile = judge_pc_or_mobile(re_title)
     allhotgoods = ALLHotGoods.objects.all()
+    if is_mobile:
+        return render(request, 'goods_m.html', {'hotgoods': allhotgoods})
     return render(request, 'goods.html', {'hotgoods': allhotgoods})
 
 def dingdans(request):
+    re_title = request.META.get("HTTP_USER_AGENT")
+    is_mobile = judge_pc_or_mobile(re_title)
     dingdan = Dingdans.objects.all()
+    if is_mobile:
+        return render(request, 'goods_m.html', {'dingdan': dingdan})
     return render(request, 'goods.html', {'dingdan': dingdan})
 
 def allgoods(request):
+    
+
     if request.POST:
         if 'good_save' in request.POST:
             try:
@@ -219,6 +273,8 @@ def allgoods(request):
     return render(request, 'UploadGoods.html')
 
 def showAllGoodPage(request):
+    re_title = request.META.get("HTTP_USER_AGENT")
+    is_mobile = judge_pc_or_mobile(re_title)
     allgoods = AllGoods.objects.all()
     goodRequest['allNum'] = allgoods.values().count()
     goodRequest['allPage'] = allgoods.values().count()//20 + 1
@@ -227,9 +283,13 @@ def showAllGoodPage(request):
     page_obj = page.get_page(1)
     goodRequest = {}
     goodRequest['category'] = 'All'
+    if is_mobile:
+        return render(request, 'goods_m.html', {'goods': page_obj, 'res':goodRequest})
     return render(request, 'goods.html', {'goods': page_obj, 'res':goodRequest})
 
 def showAllGood(request, category):
+    re_title = request.META.get("HTTP_USER_AGENT")
+    is_mobile = judge_pc_or_mobile(re_title)
     random.seed(int(time.time())//86400)
     if request.method == "POST":
         # 导航栏搜索功能
@@ -244,11 +304,15 @@ def showAllGood(request, category):
                 # 商品ID搜索
                 try:
                     info = AllGoods.objects.get(Product_Number=goodid)
+                    if is_mobile:
+                        return render(request, 'goodInfo_m.html',{'info':info})
                     return render(request, 'goodInfo.html',{'info':info})
                 except:
                     random.shuffle(allgoods)
                     page = Paginator(allgoods, 20)
                     page_obj = page.get_page(1)
+                    if is_mobile:
+                        return render(request, 'goods_m.html', {'goods': page_obj, 'ERROR':'商品ID错误'})
                     return render(request, 'goods.html', {'goods': page_obj, 'ERROR':'商品ID错误'})
             else:
                 # 关键字搜索
@@ -283,6 +347,8 @@ def showAllGood(request, category):
                 random.shuffle(newgoods)
                 page = Paginator(newgoods, 20)
                 page_obj = page.get_page(1)
+                if is_mobile:
+                    return render(request, 'goods_m.html', {'goods': page_obj, 'goodid':goodid})
                 return render(request, 'goods.html', {'goods': page_obj, 'goodid':goodid})
 
         elif 'more_search' in request.POST:
@@ -358,6 +424,8 @@ def showAllGood(request, category):
             page_obj = page.get_page(1)
             res['allNum'] = len(newgoods)
             res['allPage'] = len(newgoods)//20 + 1
+            if is_mobile:
+                return render(request, 'goods_m.html', {'goods': page_obj, 'res':res})
             return render(request, 'goods.html', {'goods': page_obj, 'res':res})
         elif ('page_index_previous' in request.POST) or ('page_index_0001' in request.POST) or ('page_index_0002' in request.POST) or ('page_index_0003' in request.POST) or ('page_index_0004' in request.POST) or ('page_index_0005' in request.POST) or ('page_index_next' in request.POST):
             # print('进入函数')
@@ -468,6 +536,8 @@ def showAllGood(request, category):
             page_obj = page.get_page(nowPageIndex)
             res['allNum'] = len(newgoods)
             res['allPage'] = len(newgoods)//20 + 1
+            if is_mobile:
+                return render(request, 'goods_m.html', {'goods': page_obj, 'res':res})
             return render(request, 'goods.html', {'goods': page_obj, 'res':res})
     elif request.method == "GET":
         # 左侧栏点击搜索
@@ -528,6 +598,8 @@ def showAllGood(request, category):
         res['category'] = category
         res['allNum'] = len(newgoods)
         res['allPage'] = len(newgoods)//20 + 1
+        if is_mobile:
+            return render(request, 'goods_m.html', {'goods': page_obj, 'res':res})
         return render(request, 'goods.html', {'goods': page_obj, 'res':res})
 
     else:
@@ -535,6 +607,8 @@ def showAllGood(request, category):
         random.shuffle(allgoods)
         page = Paginator(allgoods, 20)
         page_obj = page.get_page(1)
+        if is_mobile:
+            return render(request, 'goods_m.html', {'goods': page_obj})
         return render(request, 'goods.html', {'goods': page_obj})
 
 
@@ -552,6 +626,8 @@ def keywords(words, items):
 
 
 def goodInfo(request):
+    re_title = request.META.get("HTTP_USER_AGENT")
+    is_mobile = judge_pc_or_mobile(re_title)
     if request.method == "GET" and request.GET:
         id = request.GET.get('goodid', None)
         goodinfo = AllGoods.objects.filter(Product_Number = id).first()
@@ -582,7 +658,8 @@ def goodInfo(request):
         else:
             for i in range(len(allgoods)):
                 bottomGoods.append(allgoods[i])
-
+        if is_mobile:
+            return render(request, 'goodInfo_m.html', {'info': goodinfo, 'goods':bottomGoods})
         return render(request, 'goodInfo.html', {'info': goodinfo, 'goods':bottomGoods})
     if request.method == "POST":
         # 商品询问信息留言
